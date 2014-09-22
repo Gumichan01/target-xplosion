75b50cd94ac9e25c437a46dd53f9d54d30c875c4
luxon.jean.pierre@gmail.com <luxon.jean.pierre@gmail.com@bc227460-dfc6-808f-0672-7c8f1622ba47>
2014-09-22 18:25:44 +0000

    I forgot the Basic_enemies files. Sorry. Now the source code is compilable

---
+++ src/entities/Basic_Enemy.cpp
@@ -0,0 +1,97 @@
+
+
+/*
+*
+*	Copyright (C)  Luxon Jean-Pierre
+*	gumichan01.olympe.in
+*
+*
+*	Luxon Jean-Pierre (Gumichan01)
+*	luxon.jean.pierre@gmail.com
+*
+*/
+
+/**
+*	@file
+*	@brief
+*	@author Luxon Jean-Pierre(Gumichan01)
+*	@version
+*	@date
+*
+*
+*
+*/
+
+#include "Basic_Enemy.h"
+
+
+
+Missile * Basic_Enemy::shoot(MISSILE_TYPE m_type)
+{
+
+    SDL_Rect pos_mis;   // the missiles position
+    Speed sp_mis;       // the missiles speed
+
+
+    switch(m_type)
+    {
+        case MISSILE_TYPE::BASIC_MISSILE_TYPE :
+        {
+            pos_mis.x = position.x + ( (position.w - MISSILE_HEIGHT)/ 2);
+            pos_mis.y = position.y + ( (position.h - MISSILE_HEIGHT)/ 2);
+
+            pos_mis.w = MISSIlE_WIDTH;
+            pos_mis.h = MISSILE_HEIGHT;
+            sp_mis = {-MISSILE_SPEED,0};
+
+            return ( new Basic_missile(attack_val, LX_graphics::load_image("image/missile2.png"),NULL,&pos_mis,&sp_mis) );
+
+        }break;
+
+
+        case MISSILE_TYPE::ROCKET_TYPE : // rocket
+        {
+            pos_mis.x = position.x + ( (position.w - ROCKET_HEIGHT)/ 2);
+            pos_mis.y = position.y + ( (position.h - ROCKET_HEIGHT)/ 2);
+
+            pos_mis.w = ROCKET_WIDTH;
+            pos_mis.h = ROCKET_HEIGHT;
+            sp_mis = {-ROCKET_SPEED,0};
+
+            return ( new Rocket(attack_val, LX_graphics::load_image("image/rocket_TX2.png"),NULL,&pos_mis,&sp_mis) );
+
+        }break;
+
+
+        case MISSILE_TYPE::LASER_TYPE : // laser
+        {
+            pos_mis.x = position.x + ( (position.w - LASER_HEIGHT)/ 2);
+            pos_mis.y = position.y + ( (position.h - LASER_HEIGHT)/ 2);
+
+            pos_mis.w = LASER_WIDTH;
+            pos_mis.h = LASER_HEIGHT;
+            sp_mis = {-LASER_SPEED,0};
+
+            return ( new Laser(attack_val, LX_graphics::load_image("image/laser2.png"),NULL,&pos_mis,&sp_mis) );
+
+        }break;
+
+
+        case MISSILE_TYPE::BOMB_TYPE : // bomb
+        {
+            pos_mis.x = position.x + ( (position.w - BOMB_HEIGHT)/ 2);
+            pos_mis.y = position.y + ( (position.h - BOMB_HEIGHT)/ 2);
+
+            pos_mis.w = BOMB_WIDTH;
+            pos_mis.h = BOMB_HEIGHT;
+            sp_mis = {-BOMB_SPEED,0};
+
+            return ( new Bomb(attack_val, LX_graphics::load_image("image/bomb2.png"),NULL,&pos_mis,&sp_mis) );
+
+        }break;
+
+        default : return NULL;
+    }
+
+    return NULL;
+}
---
+++ src/entities/Basic_Enemy.h
@@ -0,0 +1,66 @@
+#ifndef BASIC_ENEMY_H_INCLUDED
+#define BASIC_ENEMY_H_INCLUDED
+
+
+
+/*
+*
+*	Copyright (C)  Luxon Jean-Pierre
+*	gumichan01.olympe.in
+*
+*
+*	Luxon Jean-Pierre (Gumichan01)
+*	luxon.jean.pierre@gmail.com
+*
+*/
+
+/**
+*	@file Basic_Enemy.h
+*	@brief The basic enemy class
+*	@author Luxon Jean-Pierre(Gumichan01)
+*	@version 0.2
+*	@date September 19th, 2014
+*
+*
+*
+*/
+
+#include "Enemy.h"
+
+
+class Basic_Enemy: public Enemy
+{
+
+
+    public:
+
+    Basic_Enemy(unsigned int hp, unsigned int att, unsigned int sh,
+            SDL_Surface *image, Mix_Chunk *audio,
+                Sint16 x, Sint16 y, Uint16 w, Uint16 h,int dX, int dY)
+        : Enemy(hp,att,sh,image,audio,x,y,w,h,dX,dY)
+    {
+        strat = new Basic_strategy(this);
+    }
+
+    Basic_Enemy(unsigned int hp, unsigned int att, unsigned int sh,
+            SDL_Surface *image, Mix_Chunk *audio, SDL_Rect *rect,Speed *sp)
+        : Enemy(hp,att,sh,image,audio,rect,sp)
+    {
+        strat = new Basic_strategy(this);
+    }
+
+    Missile * shoot(MISSILE_TYPE m_type);
+
+
+    ~Basic_Enemy(){}
+
+};
+
+
+
+
+
+
+
+
+#endif // BASIC_ENEMY_H_INCLUDED
