4bf34154390f9845f336b3d456cda718296a6b7a
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-07-15 21:10:45 +0200

    Missiles die when they are out of the visible area

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -202,7 +202,7 @@ GAME_STATUS Game::loop(ResultInfo *info)
     long ticks;
 
     mainMusic->volume(MIX_MAX_VOLUME - 32);
-    mainMusic->play();
+    //mainMusic->play();
     LX_Mixer::allocateChannels(64);
 
     player_missiles.reserve(RESERVE);
@@ -703,7 +703,9 @@ void Game::status(void)
             continue;
 
         if(enemies_missiles[k]->getX() <= (-(enemies_missiles[k]->getWidth()) -1)
-                || enemies_missiles[k]->getX() >= game_Xlimit)
+           || enemies_missiles[k]->getX() >= game_Xlimit
+           || enemies_missiles[k]->getY() <= (-(enemies_missiles[k]->getHeight()) -1)
+           || enemies_missiles[k]->getY() >= game_Ylimit)
             enemies_missiles[k]->die();
         else
             enemies_missiles[k]->move();
@@ -872,7 +874,7 @@ bool Game::generateEnemy(void)
                 case 22 :
                 {
                     // Boss is comming ( T_T)
-                    alarm->play();
+                    //alarm->play();
                 }
                 break;
 
@@ -880,7 +882,7 @@ bool Game::generateEnemy(void)
                 {
                     bossMusic = LX_Mixer::loadMusic("audio/boss01.ogg");
                     LX_Mixer::haltChannel(-1);
-                    bossMusic->play();
+                    //bossMusic->play();
                     enemies.push_back(new Boss00(data.hp,data.att,data.sh,
                                                  LX_Graphics::loadTextureFromFile("image/boss00_sprite.png",0),
                                                  LX_Mixer::loadSample("audio/explosion.wav"),
