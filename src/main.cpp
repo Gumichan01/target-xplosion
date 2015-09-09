69478ecfe43aa93a193ab846e8fdbfb8283c7dc6
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-09-09 23:12:47 +0200

    Development of version 0.5

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@ int main (int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    window = new LX_Window("Target Xplosion v0.4",LX_WINDOW_RENDERING);
+    window = new LX_Window("Target Xplosion v0.5-dev",LX_WINDOW_RENDERING);
     id = LX_Graphics::LX_WindowManager::getInstance()->addWindow(window);
 
     if(id == -1)
