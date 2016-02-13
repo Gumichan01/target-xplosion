8ec4681d9ad9fc63aee2aa13984a11ad2b522fc3
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-02-13 16:51:11 +0100

    Defined the resource manager

---
+++ src/resources/ResourceManager.hpp
@@ -0,0 +1,60 @@
+
+
+/*
+*	Target_Xplosion - The classic shoot'em up video game
+*	Copyright (C) 2016  Luxon Jean-Pierre
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*	Luxon Jean-Pierre (Gumichan01)
+*	website : gumichan01.olympe.in
+*	mail : luxon.jean.pierre@gmail.com
+*/
+
+#ifndef RESOURCEMANAGER_HPP_INCLUDED
+#define RESOURCEMANAGER_HPP_INCLUDED
+
+/**
+*	@file ResourceManager.hpp
+*	@brief Define the ressource handler
+*	@author Luxon Jean-Pierre(Gumichan01)
+*
+*/
+
+class EnemyResourcesManager;
+
+class ResourceManager
+{
+    EnemyResourcesManager *enemyManager;
+
+public:
+
+    ResourceManager();
+    ~ResourceManager();
+};
+
+#endif // RESOURCEMANAGER_HPP_INCLUDED
+
+
+
+
+
+
+
+
+
+
+
+
+
