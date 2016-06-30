7a5c3f54fc8233253804f24599439ff2c8e3b5e6
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-06-30 21:31:40 +0200

    Added a visitor for the player

---
+++ src/entities/PlayerVisitor.hpp
@@ -0,0 +1,57 @@
+
+/*
+*	Target_Xplosion - The classic shoot'em up video game
+*	Copyright (C) 2016  Luxon Jean-Pierre
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*	Luxon Jean-Pierre (Gumichan01)
+*	website : gumichan01.kappatau.fr
+*	mail : luxon.jean.pierre@gmail.com
+*/
+
+#ifndef PLAYERVISITOR_HPP_INCLUDED
+#define PLAYERVISITOR_HPP_INCLUDED
+
+/**
+*	@file PlayerVisitor.hpp
+*	@brief The visitor of the player (header)
+*	@author Luxon Jean-Pierre(Gumichan01)
+*
+*/
+
+namespace LX_Physics
+{
+struct LX_Point;
+};
+
+class PlayerVisitor
+{
+    PlayerVisitor(PlayerVisitor& p);
+    PlayerVisitor& operator =(PlayerVisitor& p);
+
+protected:
+
+    int last_player_x;
+    int last_player_y;
+
+public:
+
+    PlayerVisitor();
+
+    void visit(LX_Physics::LX_Point& last);
+    ~PlayerVisitor() = default;
+};
+
+#endif // PLAYERVISITOR_HPP_INCLUDED
