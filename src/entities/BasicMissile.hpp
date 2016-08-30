a7661c7dfd5254a7006dc63fcca189e2724baa72
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-08-30 13:46:52 +0200

    Refactored the comments of the source code

--- src/entities/BasicMissile.hpp
+++ src/entities/BasicMissile.hpp
@@ -1,24 +1,24 @@
 
 /*
-*	Target_Xplosion - A classic shoot'em up video game
-*	Copyright (C) 2016  Luxon Jean-Pierre
+*   Target_Xplosion - A classic shoot'em up video game
+*   Copyright (C) 2016  Luxon Jean-Pierre
 *
-*	This program is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 *
-*	This program is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*	GNU General Public License for more details.
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
 *
-*	You should have received a copy of the GNU General Public License
-*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
-*	Luxon Jean-Pierre (Gumichan01)
-*	website : gumichan01.kappatau.fr
-*	mail : luxon.jean.pierre@gmail.com
+*   Luxon Jean-Pierre (Gumichan01)
+*   website : gumichan01.kappatau.fr
+*   mail : luxon.jean.pierre@gmail.com
 */
 
 #ifndef BASICMISSILE_H_INCLUDED
@@ -32,13 +32,12 @@ class BasicMissile: public Missile
 
 public:
 
-    BasicMissile(unsigned int pow, LX_Graphics::LX_Sprite *image, LX_Mixer::LX_Sound *audio,
-                 SDL_Rect& rect,LX_Physics::LX_Vector2D& sp)
+    BasicMissile(unsigned int pow, LX_Graphics::LX_Sprite *image,
+                 LX_Mixer::LX_Sound *audio, SDL_Rect& rect,
+                 LX_Physics::LX_Vector2D& sp)
         : Missile(pow, 1, image, audio, rect, sp) {}
 
     ~BasicMissile() {}
 };
 
 #endif // BASICMISSILE_H_INCLUDED
-
-
