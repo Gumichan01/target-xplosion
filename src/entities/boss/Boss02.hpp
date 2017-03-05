6d39cfe14a1d135d68272eb743e539b2df1e5863
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-03-05 21:23:44 +0100

    new Boss02 object

---
+++ src/entities/boss/Boss02.hpp
@@ -0,0 +1,55 @@
+
+/*
+*   Target_Xplosion - A classic shoot'em up video game
+*   Copyright © 2017  Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   website: https://gumichan01.github.io/
+*   mail: luxon.jean.pierre@gmail.com
+*/
+
+#ifndef BOSS02_HPP_INCLUDED
+#define BOSS02_HPP_INCLUDED
+
+#include "Boss.hpp"
+
+
+namespace LX_Graphics
+{
+class LX_Sprite;
+};
+
+
+class Boss02: public Boss
+{
+
+public:
+
+    explicit Boss02(unsigned int hp, unsigned int att, unsigned int sh,
+                    LX_Graphics::LX_Sprite *image, LX_Mixer::LX_Sound *audio,
+                    int x, int y, int w, int h, float vx, float vy);
+
+    virtual void fire();
+    virtual void strategy();
+    virtual void move();
+    //virtual void collision(Missile *mi);
+    //virtual void collision(Player *play);
+    //virtual void die();
+
+    ~Boss02();
+};
+
+#endif // BOSS02_HPP_INCLUDED
