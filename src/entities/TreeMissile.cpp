ec58c9daa6c3cbe2b20ff57423fc1243a377890e
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-03-05 23:08:04 +0100

    new missile type: TreeMissile

---
+++ src/entities/TreeMissile.cpp
@@ -0,0 +1,56 @@
+
+/*
+*   Target_Xplosion - A classic shoot'em up video game
+*   Copyright © 2017  Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   website: https://gumichan01.github.io/
+*   mail: luxon.jean.pierre@gmail.com
+*/
+
+#include "TreeMissile.hpp"
+
+#include "../game/Game.hpp"
+
+#include <LunatiX/LX_Timer.hpp>
+
+namespace
+{
+const unsigned int TREE_DELAY = 500;
+};
+
+
+TreeMissile::TreeMissile(unsigned int pow, LX_Graphics::LX_Sprite *image,
+                         LX_Mixer::LX_Sound *audio, LX_AABB& rect,
+                         LX_Physics::LX_Vector2D& sp)
+    : BasicMissile(pow, image, audio, rect, sp), t(LX_Timer::getTicks()) {}
+
+
+void TreeMissile::move()
+{
+    Missile::move();
+
+    if((LX_Timer::getTicks() - t) > TREE_DELAY)
+    {
+        Game *g = Game::getInstance();
+        LX_Physics::LX_Vector2D v = speed;
+        v.vy = -v.vy;
+        g->acceptEnemyMissile(new BasicMissile(power, graphic, sound, position, v));
+        t = LX_Timer::getTicks();
+    }
+}
+
+TreeMissile::~TreeMissile() {}
---
+++ src/entities/TreeMissile.hpp
@@ -0,0 +1,44 @@
+
+/*
+*   Target_Xplosion - A classic shoot'em up video game
+*   Copyright © 2017  Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   website: https://gumichan01.github.io/
+*   mail: luxon.jean.pierre@gmail.com
+*/
+
+#ifndef TREEMISSILE_HPP_INCLUDED
+#define TREEMISSILE_HPP_INCLUDED
+
+#include "BasicMissile.hpp"
+
+class TreeMissile: public BasicMissile
+{
+    unsigned int t;
+
+
+public:
+
+    TreeMissile(unsigned int pow, LX_Graphics::LX_Sprite *image,
+                 LX_Mixer::LX_Sound *audio, LX_AABB& rect,
+                 LX_Physics::LX_Vector2D& sp);
+
+    virtual void move();
+    virtual ~TreeMissile();
+};
+
+#endif // TREEMISSILE_HPP_INCLUDED
