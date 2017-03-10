b9354aeb6f428522c93e795a20beecdc94da7a87
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-03-10 16:19:06 +0100

    Boss02: danmaku — part #3
    Two MegaBullets in a row
    
    --

--- src/entities/boss/Boss02.cpp
+++ src/entities/boss/Boss02.cpp
@@ -253,13 +253,21 @@ void Boss02::danmaku()
     Game *g = Game::getInstance();
     ResourceManager *rm = ResourceManager::getInstance();
     LX_Vector2D v(BOSS02_MSTRAT4_SPEED, speed.vy/2.0f);
-    LX_AABB b = {position.x + BOSS02_MSTRAT4_BULLET_XOFF,
-                 position.y + BOSS02_MSTRAT4_BULLET_YOFF,
-                 BOSS02_MSTRAT4_BULLET_WIDTH, BOSS02_MSTRAT4_BULLET_HEIGHT
-                };
+
+    LX_AABB b1 = {position.x + BOSS02_MSTRAT4_BULLET_XOFF,
+                  position.y + BOSS02_MSTRAT4_BULLET_YOFF,
+                  BOSS02_MSTRAT4_BULLET_WIDTH, BOSS02_MSTRAT4_BULLET_HEIGHT
+                 };
+    LX_AABB b2 = {position.x + BOSS02_MSTRAT3_ROCKET_XOFF,
+                  position.y + BOSS02_MSTRAT3_ROCKET_YOFF,
+                  BOSS02_MSTRAT4_BULLET_WIDTH, BOSS02_MSTRAT4_BULLET_HEIGHT
+                 };
 
     LX_Graphics::LX_Sprite *s = rm->getResource(RC_MISSILE, BOSS02_MSTRAT4_BULLET_ID);
-    g->acceptEnemyMissile(new MegaBullet(attack_val, s, nullptr, b, v, BOSS02_MSTRAT44_SPEED));
+    g->acceptEnemyMissile(new MegaBullet(attack_val, s, nullptr, b1, v,
+                                         BOSS02_MSTRAT44_SPEED));
+    g->acceptEnemyMissile(new MegaBullet(attack_val, s, nullptr, b2, v,
+                                         BOSS02_MSTRAT44_SPEED));
 }
 
 void Boss02::fire()
