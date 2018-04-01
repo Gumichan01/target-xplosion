765fc4d4ef00c1bf14e8e848341760b92d724ce9
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-04-01 10:31:54 +0200

    tx: miscellaneous classes/functions/idioms

---
+++ src/utils/misc.hpp
@@ -0,0 +1,48 @@
+
+/*
+*   Copyright © 2018 Luxon Jean-Pierre
+*   https://gumichan01.github.io/
+*
+*   This library is under the MIT license
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   luxon.jean.pierre@gmail.com
+*/
+
+/**
+*   @file misc.hpp
+*   Miscellaneous classes/functions/idoms
+*/
+
+
+#include <iostream>
+#include <sstream>
+
+namespace misc
+{
+// This implemantation exists in C++11, but is missing in GCC 4.9
+template <typename T>
+std::string to_string(const T& v)
+{
+    std::ostringstream ss;
+    ss << v;
+    return ss.str();
+}
+
+// This implemantation exists in C++17, but I am still using C++11, so I need it
+template< class ForwardIt, class T >
+ForwardIt remove(ForwardIt first, ForwardIt last, const T& value)
+{
+    first = std::find(first, last, value);
+    if (first != last)
+    {
+        for(ForwardIt i = first; ++i != last; )
+        {
+            if (!(*i == value))
+                *first++ = std::move(*i);
+        }
+    }
+    return first;
+}
+
+}
