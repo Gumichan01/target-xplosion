1d46902b5db3e0e815c45d05036c646430ddcc72
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-04-08 13:31:28 +0200

    player: reduce the speed — not here

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -731,9 +731,4 @@ void Player::setShield(bool sh) noexcept
 void Player::notifySlow(bool slow) noexcept
 {
     slow_mode = slow;
-
-    if(slow)
-    {
-        speed /= fbox(PLAYER_SPEED_RATIO);
-    }
 }
