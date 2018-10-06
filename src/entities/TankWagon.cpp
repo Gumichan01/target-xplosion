1b4a6c9847fd017b2f497fe1b8f0234793a51c29
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-10-06 11:27:09 +0200

    Makafile: new enemy — TnankWagon
    TankWagon is an enemy in the fourth level.
    It shoots multiple waves of bombs.
    
    --

---
+++ src/entities/TankWagon.cpp
@@ -0,0 +1,25 @@
+
+/*
+*   Target_Xplosion - A classic shoot'em up video game
+*   Copyright © 2018 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*   Luxon Jean-Pierre (Gumichan01)
+*   website: https://gumichan01.github.io/
+*   mail: luxon.jean.pierre@gmail.com
+*/
+
+
+#include "TankWagon.hpp"
